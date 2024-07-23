cfd9878f248c292c4bb916f5a8363f5cdd908660
alymurtazamemon <alimurtaza-assistant@outlook.com>
2024-07-23 18:51:06 +0500

    test(subscription-vrf-unit-test): test fulfillRandomWords function

--- test/unit/SubscriptionVRFUnitTest.t.sol
+++ test/unit/SubscriptionVRFUnitTest.t.sol
@@ -10,9 +10,12 @@ import {DeploySubscriptionVRFV2_5} from "../../script/deploy/DeploySubscriptionV
 import {SubscriptionVRF_HelperConfig} from "../../script/helper-configs/SubscriptionVRF_HelperConfig.s.sol";
 import {SubscriptionBasedVRFConsumer} from "../../src/SubscriptionBasedVRFConsumer.sol";
 
+import {VRFCoordinatorV2_5Mock} from "@chainlink/contracts/src/v0.8/vrf/mocks/VRFCoordinatorV2_5Mock.sol";
+
 contract SubscriptionVRFUnitTest is Test {
     SubscriptionBasedVRFConsumer vrfConsumer;
     SubscriptionVRF_HelperConfig helperConfig;
+    VRFCoordinatorV2_5Mock vrfCoordinatorV2_5;
 
     event RequestSent(uint256 requestId, uint32 numWords);
 
@@ -26,8 +29,11 @@ contract SubscriptionVRFUnitTest is Test {
     }
 
     function setUp() external onlyLocalChain {
+        address vrfCoordinatorV2_5Address;
+
         DeploySubscriptionVRFV2_5 deployer = new DeploySubscriptionVRFV2_5();
-        (vrfConsumer, helperConfig) = deployer.run();
+        (vrfConsumer, helperConfig, vrfCoordinatorV2_5Address) = deployer.run();
+        vrfCoordinatorV2_5 = VRFCoordinatorV2_5Mock(vrfCoordinatorV2_5Address);
     }
 
     function test_onlyOnwerCanCallRequestRandomNumber() external {
@@ -49,5 +55,27 @@ contract SubscriptionVRFUnitTest is Test {
         assertEq(fulfilled, false);
         assertEq(vrfConsumer.getRequestIds()[0], requestId);
         assertEq(vrfConsumer.getLastRequestId(), requestId);
+    }
+
+    function test_fulfillRandomWords_RevertIfRequestIdDoesntExist() external {
+        vm.expectRevert(VRFCoordinatorV2_5Mock.InvalidRequest.selector);
+
+        vrfCoordinatorV2_5.fulfillRandomWords(1, address(vrfConsumer));
+    }
+
+    function test_fulfillRandomWords() external {
+        vm.startPrank(msg.sender);
+
+        uint256 requestId = vrfConsumer.requestRandomNumber();
+
+        vm.stopPrank();
+
+        vrfCoordinatorV2_5.fulfillRandomWords(requestId, address(vrfConsumer));
+
+        (bool fulfilled, uint256[] memory randomWords) = vrfConsumer
+            .getRequestStatus(requestId);
+
+        assertEq(fulfilled, true);
+        assertEq(randomWords.length, 1);
     }
 }
